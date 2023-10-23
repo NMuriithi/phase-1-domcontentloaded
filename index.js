369c469ab4bdac5a92cf32cdf0a6aa7a8831b627
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-23 15:23:11 -0700

    This is really cool!

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let newText = document.querySelector('#text')
+    newText.innerHTML = "This is really cool!"
+  });
+  
+  
